13e7bf47cb873df49029aca3b1dfc5894c17e863
Yen-Chieh Huang <lashhw81@gmail.com>
2024-11-30 17:53:56 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -9,9 +9,8 @@ fi
 cmake -S src -B build
 cmake --build build
 
-clang -Xclang -disable-O0-optnone -fno-discard-value-names -ffp-contract=off -O0 -S -emit-llvm $1 -o ir.ll
-opt -S -passes="mem2reg" ir.ll -o ir_noopt.ll
-opt -load-pass-plugin build/DataflowAnalyzerPass.so -passes=dataflow-analyzer --disable-output ir_noopt.ll 2> ir_noopt.dot
+clang -Xclang -disable-O0-optnone -fno-discard-value-names -ffp-contract=off -O0 -S -emit-llvm $1 -o - | opt -S -passes="mem2reg" -o noopt.ll
+opt -load-pass-plugin build/DataflowAnalyzerPass.so -passes=dataflow-analyzer --disable-output noopt.ll 2> noopt.dot
 
-clang -fno-discard-value-names -ffp-contract=off -O3 -fno-slp-vectorize -S -emit-llvm $1 -o ir_opt.ll
-opt -load-pass-plugin build/DataflowAnalyzerPass.so -passes=dataflow-analyzer --disable-output ir_opt.ll 2> ir_opt.dot
+clang -fno-discard-value-names -ffp-contract=off -O3 -fno-slp-vectorize -S -emit-llvm $1 -o opt.ll
+opt -load-pass-plugin build/DataflowAnalyzerPass.so -passes=dataflow-analyzer --disable-output opt.ll 2> opt.dot
